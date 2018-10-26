8bc3226ccb8d11a96a7ca55770a7ad38e170fcad
Manrique Buso - Garcia <manrique.buso@gmail.com>
2018-10-26 23:34:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
-function destructivelyAppendKitten(array) {
-array.shift()
+function destructivelyAppendKitten(name) {
+kittens.push()
 return array
 }
